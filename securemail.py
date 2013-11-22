b9076b9afb06c32503127650140f7fe17960ea63
BoppreH <boppreh@gmail.com>
2013-11-22 16:23:02 -0200

    Refactor

--- securemail.py
+++ securemail.py
@@ -1,72 +1,52 @@
-entities = {}
+class InternalNetwork(object):
+    def __init__(self):
+        self.entities = {}
 
-def register_mail(name, entity):
-    print('Registered {}'.format(name))
-    entities[name] = entity
+    def register(self, name, entity):
+        print('Registered {}'.format(name))
+        self.entities[name] = entity
 
-def send_mail(name, message):
-    print('to: {}. "{}"\n'.format(name, message))
-    entities[name].receive(message)
-
-class MockKey(object):
-    def __init__(self, char, is_private):
-        self.char = char
-        self.is_private = is_private
-
-    def encrypt(self, message):
-        return self.char + ('-' if self.is_private else '+') + message
-
-    def decrypt(self, message):
-        assert message[0] == self.char, 'Message should start with {}, not {} ({}).'.format(self.char, message[0], message)
-        expected_symbol = ('+' if self.is_private else '-')
-        assert message[1] == expected_symbol, 'Message should start be {}, not {} ({}).'.format(self.char, message[0], message)
-        return message[2:]
-
-    def serialize(self):
-        return self.encrypt('')
-
-    @staticmethod
-    def load(string):
-        return MockKey(string[0], string[1] == '-')
-
-class TrustedServer(object):
-    def __init__(self, privkey, pubkey):
-        self.privkey = privkey
-        self.pubkey = pubkey
+    def send(self, name, message):
+        print('to: {}. "{}"\n'.format(name, message))
+        self.entities[name].receive_raw(message)
 
 class Client(object):
-    def __init__(self, address, privkey, pubkey, trusted_root):
+    def __init__(self, address, privkey, pubkey, network):
         self.address = address
         self.privkey = privkey
         self.pubkey = pubkey
-        self.trusted_root = trusted_root
         self.pubkey_cache = {}
+        self.network = network
+        self.message_buffer = []
+
+        self.network.register(address, self)
 
     def _get_address_pubkey(self, address):
         if not address in self.pubkey_cache:
-            send_mail(address, self.address + '\n' + self.pubkey.serialize() + '\nsend me your pubkey')
+            self.network.send(address, self.address + '\n' + self.pubkey.serialize() + '\nsend me your pubkey')
         return self.pubkey_cache[address]
 
-    def receive(self, message):
+    def receive_raw(self, message):
         sender, pubkey_str, text = message.split('\n', 2)
-        pubkey = MockKey.load(pubkey_str)
+        pubkey = self.pubkey.load(pubkey_str)
         self.pubkey_cache[sender] = pubkey
 
         if text == 'send me your pubkey':
-            send_mail(sender, self.address + '\n' + self.pubkey.serialize() + '\n')
+            self.network.send(sender, self.address + '\n' + self.pubkey.serialize() + '\n')
         elif len(text):
-            print('Received:', pubkey.decrypt(self.privkey.decrypt(text)))
+            self.message_buffer.append(pubkey.decrypt(self.privkey.decrypt(text)))
+
+    def receive(self):
+        return self.message_buffer.pop(0)
 
     def send(self, address, message):
         pubkey = self._get_address_pubkey(address)
         ciphertext = pubkey.encrypt(self.privkey.encrypt(message))
-        send_mail(address, self.address + '\n' + self.pubkey.serialize() + '\n' + ciphertext)
+        self.network.send(address, self.address + '\n' + self.pubkey.serialize() + '\n' + ciphertext)
 
 if __name__ == '__main__':
-    root = TrustedServer(MockKey('r', True), MockKey('r', False))
-    
-    alice = Client('alice', MockKey('a', True), MockKey('a', False), root)
-    bob = Client('bob', MockKey('b', True), MockKey('b', False), root)
+    alice = Client('alice', MockKey('a', True), MockKey('a', False), IntervalNetwork)
+    bob = Client('bob', MockKey('b', True), MockKey('b', False), IntervalNetwork)
 
     register_mail('alice', alice)
     register_mail('bob', bob)
--- test_securemail.py
+++ test_securemail.py
@@ -1,8 +1,40 @@
 import unittest
+from securemail import Client, InternalNetwork
 
-class T(unittest.TestCase):
-    def test_a(self):
-        self.assertTrue(True)
+class MockKey(object):
+    def __init__(self, char, is_private):
+        self.char = char
+        self.is_private = is_private
+
+    def encrypt(self, message):
+        return self.char + ('-' if self.is_private else '+') + message
+
+    def decrypt(self, message):
+        assert message[0] == self.char, 'Message should start with {}, not {} ({}).'.format(self.char, message[0], message)
+        expected_symbol = ('+' if self.is_private else '-')
+        assert message[1] == expected_symbol, 'Message should start be {}, not {} ({}).'.format(self.char, message[0], message)
+        return message[2:]
+
+    def serialize(self):
+        return self.encrypt('')
+
+    def load(self, string):
+        return MockKey(string[0], string[1] == '-')
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.network = InternalNetwork()
+
+    def make_client(self, name='alice'):
+        address = name
+        key_secret = name[0]
+        return Client(address, MockKey(key_secret, True), MockKey(key_secret, False), self.network)
+
+    def test_self_connection(self):
+        c = self.make_client()
+        message = 'test_message'
+        c.send(c.address, message)
+        self.assertTrue(c.receive() == message)
 
 if __name__ == '__main__':
     unittest.main()
